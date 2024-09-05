42f4726c61e94e550d2b928a6fff1ead07dbf9b1
andrestorres29 <p315463@uach.mx>
2024-09-05 16:32:12 -0600

    Update codigohousing.py

--- codigohousing.py
+++ codigohousing.py
@@ -41,27 +41,20 @@ pipeline = joblib.load('pipeline.sav')
 
 
 def main():
-    st.title("California Housing Prices")
-    st.markdown(
-    """
-    <style>
-    .css-1v0mbdj {
-        background-color: #009dcd;  /* Cambia el color de fondo del encabezado */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-    longitude = st.number_input("Longitude",min_value=-124.35,max_value=-114.31,value="min")
-    latitude = st.number_input("Latitude",min_value=32.54,max_value=41.95,value="min")
-    house_med_age = st.number_input("Housing Median Age",min_value=1.0,max_value=52.0,value="min")
-    total_rooms = st.number_input("Total Rooms",min_value=2.0,max_value=39320.0,value="min")
-    total_bedrooms = st.number_input("Total Bedrooms",min_value=1.0,max_value=6445.0,value="min")
-    population = st.number_input("Population",min_value=3.0,max_value=35682.0,value="min")
-    households = st.number_input("Households",min_value=1.0,max_value=6082.0,value="min")
-    median_income = st.number_input("Median Income",min_value=0.499,max_value=15.0001,value="min")
-    median_house_value = st.number_input("Median House Value",min_value=14999.00,max_value=500001.0,value="min")
-    ocean_proximity = st.selectbox("Ocean Proximity",["<1H OCEAN","INLAND","NEAR OCEAN","NEAR BAY","ISLAND"])
+    st.title("Precio de las casas en el estado de California")
+    col1, col2 = st.columns(2)
+    with col1:
+        longitude = st.number_input("Longitude",min_value=-124.35,max_value=-114.31,value="min")
+        latitude = st.number_input("Latitude",min_value=32.54,max_value=41.95,value="min")
+        house_med_age = st.number_input("Housing Median Age",min_value=1.0,max_value=52.0,value="min")
+        total_rooms = st.number_input("Total Rooms",min_value=2.0,max_value=39320.0,value="min")
+        total_bedrooms = st.number_input("Total Bedrooms",min_value=1.0,max_value=6445.0,value="min")
+    with col2:
+        population = st.number_input("Population",min_value=3.0,max_value=35682.0,value="min")
+        households = st.number_input("Households",min_value=1.0,max_value=6082.0,value="min")
+        median_income = st.number_input("Median Income",min_value=0.499,max_value=15.0001,value="min")
+        median_house_value = st.number_input("Median House Value",min_value=14999.00,max_value=500001.0,value="min")
+        ocean_proximity = st.selectbox("Ocean Proximity",["<1H OCEAN","INLAND","NEAR OCEAN","NEAR BAY","ISLAND"])
     if st.button("Predict House Pricing"):
         features = [[longitude,latitude,house_med_age,total_rooms,total_bedrooms,population,households,median_income,
                      median_house_value,ocean_proximity]]
